b56d104e799279743ff147bd3b3121a1365bb24e
Jorge Jimenez <calenocam24@gmail.com>
2020-08-13 21:28:33 -0400

    finished MVP with some tests

--- src/App.test.js
+++ src/App.test.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { render } from "@testing-library/react";
+import { render, getByText, fireEvent } from "@testing-library/react";
 import App from "./App";
 import ContactForm from "./components/ContactForm";
 
@@ -10,3 +10,67 @@ test("renders App without crashing", () => {
 test('renders ContactForm without crashing', () => {
   render(<ContactForm />);
 });
+
+test('First Name text input is working properly', () => {
+  //Arrange
+  const { getByText } = render(<ContactForm />);
+  //Act
+  getByText(/First Name/i);
+  //Assertion
+});
+
+test('It shows firstName by Id', () => {
+  //Arrange
+  const { getByTestId } = render(<ContactForm />);
+  //Act
+  const firstName = getByTestId(/firstName/i);
+  //Assertion
+  expect(firstName).toBeVisible()
+});
+
+test('renders input correctly for firstName, lastName, email, and message', () => {
+  //Arrange
+  const { getByTestId } = render(<ContactForm />);
+
+  //Act
+  const firstNameInput = getByTestId(/firstName/i);
+  const lastNameInput = getByTestId(/lastName/i);
+  const email = getByTestId(/email/i);
+  const message = getByTestId(/message/i);
+
+  fireEvent.change(firstNameInput, {
+    target: {value: "Juan" }
+  });
+
+  fireEvent.change(lastNameInput, {
+    target: {value: "Smith" }
+  });
+
+  fireEvent.change(email, {
+    target: {value: "caleno24@gmail.com" }
+  });
+  //I got this info from link https://testing-library.com/docs/example-input-event
+  fireEvent.change(message, {
+    target: {value: "Hi, Friends" }
+  });
+
+  //Assertion
+  expect(firstNameInput).toHaveValue("Juan");
+  expect(lastNameInput).toHaveValue("Smith");
+  expect(email).toHaveValue("caleno24@gmail.com");
+  expect(message).toHaveValue("Hi, Friends")
+
+});
+
+test('It Submits properly', () => {
+  //Arrange
+  const { getByTestId } = render(<ContactForm />);
+  //Act
+  const submits = getByTestId(/submit/i);
+  //Assertion
+  expect(submits).toBeInTheDocument()
+});
+
+
+
+
